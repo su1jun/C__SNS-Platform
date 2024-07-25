1a6122edb41efed59185fae616f93fc9cd6f315c
su1jun <qaxsd530@gmail.com>
2024-07-25 22:36:56 +0900

    add: layout, sidebar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,25 +1,23 @@
-import Sidebar from "./layout/Sidabar"
+import React from 'react';
+import Sidebar from "@/components/layout/Sidebar"
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
-          <div className="
-            col-span-3
-            lg:col-span-2
-            border-x-[1px]
-            border-netural-800
+          <div 
+            className="
+              col-span-3 
+              lg:col-span-2 
+              border-x-[1px] 
+              border-neutral-800
           ">
             {children}
           </div>
         </div>
-      </div>
+     </div>
     </div>
   )
 }
