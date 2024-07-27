932b624822b5307237147e6df67e0394ce55ce2a
su1jun <qaxsd530@gmail.com>
2024-07-27 23:41:34 +0900

    add: login modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app'
 
+import Layout from '@/components/layout'
+import Modal from '@/components/Modal'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-
-  <Component {...pageProps} />
-
+    <>
+      <Modal isOpen title="Test Modal"/>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
