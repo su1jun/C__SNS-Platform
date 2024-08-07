9e7a0fb5e448f966c62cd5ab67a85b821a211906
su1jun <qaxsd530@gmail.com>
2024-08-07 21:15:12 +0900

    feat: 아바타 컴포넌트 정의

---
+++ components/Avatar.tsx
@@ -0,0 +1,20 @@
+import { useCallback } from "react";
+import useUser from "@/hooks/useUser";
+
+interface AvatarProps {
+  userId: string;
+  isLarge?: boolean;
+  hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ 
+  userId, 
+  isLarge, 
+  hasBorder 
+}) => {
+  return (
+    <div></div>
+  );
+}
+ 
+export default Avatar;
