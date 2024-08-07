d6ed086c6160055caf29dc4319e14b16cad44254
su1jun <qaxsd530@gmail.com>
2024-08-07 21:44:43 +0900

    fix: 히어로섹션, 바이오그래피 부분 구현

--- components/users/UserHero.tsx
+++ components/users/UserHero.tsx
@@ -1,10 +1,36 @@
+import Image from "next/image";
+
+import useUser from "@/hooks/useUser";
+
+import Avatar from "../Avatar"
+
 interface UserHeroProps {
   userId: string;
 }
 
 const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
+  const { data: fetchedUser } = useUser(userId); // Fetch user data
+
   return ( 
-    <div></div>
+    <div>
+      <div className="bg-neutral-700 h-44 relative">
+        {fetchedUser?.coverImage && (
+          <Image // Display cover image (if)
+            src={fetchedUser.coverImage} 
+            fill 
+            alt="Cover Image" 
+            style={{ objectFit: 'cover' }}
+          />
+        )}
+        <div className="absolute -bottom-16 left-4">
+          <Avatar // Display large avatar
+            userId={userId} 
+            isLarge 
+            hasBorder 
+          /> 
+        </div>
+      </div>
+    </div>
    );
 }
  
