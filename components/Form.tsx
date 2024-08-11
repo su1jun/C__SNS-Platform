194451ce979efeb58e1b341c262ee4ed010e36e1
su1jun <qaxsd530@gmail.com>
2024-08-11 21:21:19 +0900

    fix: comment 요청 주소 추가

--- components/Form.tsx
+++ components/Form.tsx
@@ -37,7 +37,9 @@ const Form: React.FC<FormProps> = ({
     try {
       setIsLoading(true);
 
-      const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
+      const url = isComment 
+        ? `/api/comments?postId=${postId}` 
+        : '/api/posts';
 
       await axios.post(url, { body });
 
